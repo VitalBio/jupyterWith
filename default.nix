daeedae7662ebcd2416fd888d51d2bdd437ce4f8
Asa Riley <asa@vitalbio.com>
2020-11-14 12:25:01 -0500

    add jupyterhub as a dep to jupyterlab

--- default.nix
+++ default.nix
@@ -35,6 +35,7 @@ let
         python3.jupyter_contrib_core
         python3.jupyter_nbextensions_configurator
         python3.tornado
+        python3.jupterhub
       ];
 
       # JupyterLab executable wrapped with suitable environment variables.
